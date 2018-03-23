1afde4d2101ed0c24659614fd6e09bab4d496eef
Ryan <ryanalvesmartins@gmail.com>
2018-03-23 08:18:42 -0300

    modificacao
    mds

---
+++ src/trabalho/Mainclass.java
@@ -0,0 +1,17 @@
+package trabalho;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static class Disciplina{
+		
+	}
+	
+	public static class Aluno{
+		
+	}
+
+}
